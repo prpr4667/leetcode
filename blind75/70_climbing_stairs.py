203cba3aad3675ed13750ec293bcf0bd8e771df8
pratikpr <89504885+prpr4667@users.noreply.github.com>
2023-04-23 20:34:49 -0600

    [e] 70. Climbing Stairs

---
+++ blind75/70_climbing_stairs.py
@@ -0,0 +1,27 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+
+
+class Solution:
+    def climbStairs(self, n):
+        dp = [0] * n
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, n):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        print(dp)
+        return dp[n - 1]
+
+
+solution = Solution()
+n = 3
+print(solution.climbStairs(n))
