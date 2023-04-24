e0460c404f9780d230db01506b37904f084063b8
pratikpr <89504885+prpr4667@users.noreply.github.com>
2023-04-23 20:18:02 -0600

    [e] 268. Missing Number - xor

---
+++ blind75/268_missing_number.py
@@ -0,0 +1,22 @@
+# Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+# Example 1:
+
+# Input: nums = [3,0,1]
+# Output: 2
+# Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+
+class Solution:
+    def missingNumber(self, nums):
+        xor = len(nums)
+
+        for i, num in enumerate(nums):
+            xor ^= i ^ num
+
+        return xor
+
+
+solution = Solution()
+nums = [3, 0, 1, 4, 2, 6]
+print(solution.missingNumber(nums))
