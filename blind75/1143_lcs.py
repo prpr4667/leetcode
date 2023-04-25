dbee43048f50c559f83e70127fddbba4d26a87d9
pratikpr <89504885+prpr4667@users.noreply.github.com>
2023-04-25 17:14:25 -0600

    [m] 1143. Longest Common Subsequence

---
+++ blind75/1143_lcs.py
@@ -0,0 +1,33 @@
+# Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+# A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+# For example, "ace" is a subsequence of "abcde".
+# A common subsequence of two strings is a subsequence that is common to both strings.
+
+# Example 1:
+
+# Input: text1 = "abcde", text2 = "ace"
+# Output: 3
+# Explanation: The longest common subsequence is "ace" and its length is 3.
+
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        n = len(text1)
+        m = len(text2)
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(m - 1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = dp[i + 1][j + 1] + 1
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+
+        return dp[0][0]
+
+
+solution = Solution()
+text1 = "pratik"
+text2 = "patrick"
+print(solution.longestCommonSubsequence(text1, text2))
