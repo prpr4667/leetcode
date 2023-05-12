985b1cee96cabd124344e85401811da5a95fd351
pratikpr <89504885+prpr4667@users.noreply.github.com>
2023-05-12 12:12:48 -0600

    [e] 226. Invert Binary Tree - BFS

--- blind75/226_invert_BT.py
+++ blind75/226_invert_BT.py
@@ -11,3 +11,21 @@ class Solution:
         self.invertTree(root.left)
         self.invertTree(root.right)
         return root
+
+    def invertTree_BFS(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            node = q.popleft()
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+
+        return root
