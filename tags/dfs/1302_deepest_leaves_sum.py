38ac331de8e126a385ce40f469bc700a359c1bd7
pratikpr <prpr4667@colorado.edu>
2023-06-20 14:47:50 -0700

    [m] 1302. Deepest Leaves Sum

--- tags/dfs/1302_deepest_leaves_sum.py
+++ tags/dfs/1302_deepest_leaves_sum.py
@@ -13,4 +13,22 @@
 
 class Solution:
     def deepestLeavesSum(self, root) -> int:
+        _sum = max_depth = 0
+        stack = [(root, 0)]
+
+        while stack:
+            node, curr_depth = stack.pop()
+            if not node.left and not node.right:
+                if curr_depth > max_depth:
+                    max_depth = curr_depth
+                    _sum = node.val
+                elif curr_depth == max_depth:
+                    _sum += node.val
+            else:
+                if node.left:
+                    stack.append((node.left, curr_depth+1))
+                if node.right:
+                    stack.append((node.right, curr_depth+1))
         
+        return _sum
+
